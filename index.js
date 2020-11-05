49e61ad39a96f5b5a955bd18898c91af97e39f6d
jacksproul <jack.sproul3@gmail.com>
2020-11-05 14:16:51 -0700

    10.1 10/27 /game/:pk

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/hi",(req,res)=>{
     res.send("hello world")
 })
 
-app.get("/movies", (req,res)=>{
+app.get("/videogame", (req,res)=>{
     //get data from database
     db.executeQuery(`SELECT *
     FROM VideoGame
@@ -22,4 +22,27 @@ app.get("/movies", (req,res)=>{
     })
 })
 
+app.get("/videogame/:pk", (req,res) =>{
+    var pk = req.params.pk
+    // console.log("my PK: ", pk)
+
+    var myQuery = `SELECT *
+    FROM VideoGame
+    LEFT JOIN GameMode
+    ON GameMode.ModePK = VideoGame.ModeFK
+    WHERE GamePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((game) =>{
+        // console.log("Game: ", game)
+
+        if (game[0]){
+            res.send(game[0])
+        } else {res.status(404).send('bad request')}
+    })
+    .catch((error) => {
+        console.log("error in /game/pk", error)
+        res.status(500).send()
+    })
+} )
 app.listen(5000,()=>{console.log("app is running on port 5000")})
