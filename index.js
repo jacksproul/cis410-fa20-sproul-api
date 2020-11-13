8a34e1b60afc7cc82533f59ff865d9297814f1b8
jacksproul <jack.sproul3@gmail.com>
2020-11-12 17:03:38 -0700

    10.2 10/29 /signup

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const db = require('./dbConnectExec.js')
 
 const app = express();
+app.use(express.json())
 
 app.get("/hi",(req,res)=>{
     res.send("hello world")
+})
+
+app.post("/player", async (req,res)=>{
+    //res.send("creating user")
+    //console.log("request body", req.body)
+
+    var FirstName = req.body.FirstName;
+    var LastName = req.body.LastName;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    if(!FirstName || !LastName || !email || !password){
+        return res.status(400).send("bad request")
+    }
+
+    FirstName = FirstName.replace("'","''")
+    LastName = LastName.replace("'","''")
+
+    var emailCheckQuery = `SELECT email
+    FROM Player
+    WHERE email = '${email}'`
+
+    var existingUser = await db.executeQuery(emailCheckQuery)
+
+    //console.log("existing user", existingUser)
+    if(existingUser[0]){
+        return res.status(409).send('Please enter a different email')
+    }
+
+    var hashedPassword = bcrypt.hashSync(password)
+
+    var insertQuery = `INSERT INTO Player(FirstName, LastName, Email, Password)
+    VALUES('${FirstName}','${LastName}','${email}','${hashedPassword}')`
+
+    db.executeQuery(insertQuery)
+    .then(()=>{res.status(201).send})
+    .catch(()=>{
+        console.log("error in POST /player", err)
+        res.status(500).send()
+    })
 })
 
 app.get("/videogame", (req,res)=>{
